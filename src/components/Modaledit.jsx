51c254c5cfc1130481ba54dc05dca9026f3e4d6d
Tsuga-bts <tsuga.putra99@gmail.com>
2025-09-12 23:56:52 +0700

    Feat: Add Modaledit

---
+++ src/components/Modaledit.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaCheck } from "react-icons/fa6";
+import { RxReset } from "react-icons/rx";
+
+const Modaledit = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const handleOutsideClick = (e) => {
+    if (e.target.id === "modal-overlay") {
+      onClose(); // close modal kalau klik luar
+    }
+  };
+
+  return (
+    <div
+      id="modal-overlay"
+      className="fixed inset-0 flex items-center justify-center bg-black/50 z-50"
+      onClick={handleOutsideClick}>
+      {/* stopPropagation agar klik dalam modal tidak menutup */}
+      <div
+        className="bg-white p-6 rounded-2xl shadow-lg"
+        onClick={(e) => e.stopPropagation()}>
+        <div className="p-2.5 w-[635px] h-[500px] text-center text-xl font-bold">
+          EDIT CONTENT
+          <div className="p-2 text-left grid items-center gap-2">
+            Username
+            <input type="text" className="border-2 px-2 py-1" />
+          </div>
+          <div className="p-2 text-left grid items-center gap-2">
+            Email
+            <input type="email" className="border-2 px-2 py-1" />
+          </div>
+          <div className="p-2 text-left grid items-center gap-2 mb-4">
+            Role
+            <select className="border-2 px-2 py-1 bg-white">
+              <option value="">User</option>
+              <option value="">Admin</option>
+            </select>
+          </div>
+          <div className="p-2 text-left grid items-center gap-2 mb-4">
+            Status
+            <select className="border-2 px-2 py-1 bg-white">
+              <option value="">Active</option>
+              <option value="">Inactive</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex gap-2 pl-4">
+          <button
+            type="button"
+            className="cursor-pointer flex items-center gap-2 px-4 py-2 bg-red-300 hover:bg-red-400 rounded">
+            <RxReset /> Reset
+          </button>
+          <button
+            type="button"
+            className="cursor-pointer flex items-center gap-2 px-4 py-2 bg-yellow-300 hover:bg-yellow-400 rounded">
+            <FaCheck /> Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modaledit;
