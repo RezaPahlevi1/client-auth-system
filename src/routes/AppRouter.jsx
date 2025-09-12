54fa7a27a12886e379e7e6912554dbb5c4556df5
Tsuga-bts <tsuga.putra99@gmail.com>
2025-09-12 16:58:29 +0700

    Feat: Create SignupForm & Fix Some LoginForm Styling

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import AdminDashboard from "../pages/AdminDashboard";
 import UserDashboard from "../pages/UserDashboard";
+import Register from "../pages/Register";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
         <Route path="/dashboard-admin" element={<AdminDashboard />} />
         <Route path="/dashboard-user" element={<UserDashboard />} />
       </Routes>
